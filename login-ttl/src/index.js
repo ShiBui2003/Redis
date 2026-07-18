0a895adf5dffae65eea100b995f8fb1d5be9f684
ShiBui2003 <irahul.jha.007@gmail.com>
2026-07-18 21:11:54 +0530

    OTP_TTL

---
+++ login-ttl/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import Redis from "ioredis";
+
+
+const app = express();
+app.use(express.json());       
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+function otpKey(phone){
+    return `otp:${phone}`;
+}
+
+
+app.post('/otp',async(req,res)=>{
+    const{phone} = req.body;
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    await redis.set(otpKey(phone), otp, 'EX',30);//otp valio for 30 secs
+    res.json({message:'OTP SENT',otp});
+});
